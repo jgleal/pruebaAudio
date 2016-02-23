005b4398a6f8f300f811d6329893fafc3274675a
Javier G. Leal <jgleal@gmail.com>
2016-02-23 13:07:03 +0100

    primera versión de mapea4 móvil

---
+++ js/configuration.js
@@ -0,0 +1,260 @@
+/**
+ * Mapea API
+ * Version 4.0.0-SNAPSHOT
+ * Date 19-02-2016
+ */
+(function (M) {
+   /**
+    * The Mapea URL
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('MAPEA_URL', '');
+
+   /**
+    * The path to the Mapea proxy to send
+    * jsonp requests
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('PROXY_PATH', '/api/proxy');
+
+   /**
+    * The path to the Mapea proxy to send
+    * jsonp requests
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('PROXY_POST_PATH', '/proxyPost');
+
+   /**
+    * The path to the Mapea templates
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('TEMPLATES_PATH', '/files/templates/');
+
+   /**
+    * The Geosearch URL
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('GEOSEARCH_URL', 'http://geobusquedas-sigc.juntadeandalucia.es');
+
+   /**
+    * The Geosearch core
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('GEOSEARCH_CORE', 'sigc');
+
+   /**
+    * The Geosearch handler
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('GEOSEARCH_HANDLER', '/search?');
+
+   /**
+    * The Geosearch distance
+    * @const
+    * @type {int}
+    * @public
+    * @api stable
+    */
+   M.config('GEOSEARCH_DISTANCE', '600');
+
+   /**
+    * The Geosearchbylocation spatial field
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('GEOSEARCH_SPATIAL_FIELD', 'geom');
+
+   /**
+    * The Geosearch rows
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('GEOSEARCH_ROWS', '100');
+
+   /**
+    * Predefined WMC files. It is composed of URL,
+    * predefined name and context name.
+    * @type {object}
+    * @public
+    * @api stable
+    */
+   M.config('predefinedWMC', {
+      /**
+       * Predefined WMC URLs
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'urls': 'http://mapea-sigc.juntadeandalucia.es/Componente/mapConfig/contextCallejeroCache.xml,http://mapea-sigc.juntadeandalucia.es/Componente/mapConfig/contextCallejero.xml,http://mapea-sigc.juntadeandalucia.es/Componente/mapConfig/contextOrtofoto.xml,http://mapea-sigc.juntadeandalucia.es/Componente/mapConfig/contextIDEA.xml,http://mapea-sigc.juntadeandalucia.es/Componente/mapConfig/contextOrtofoto2009.xml,http://mapea-sigc.juntadeandalucia.es/Componente/mapConfig/callejero2011cache.xml,http://mapea-sigc.juntadeandalucia.es/Componente/mapConfig/ortofoto2011cache.xml,http://mapea-sigc.juntadeandalucia.es/Componente/mapConfig/hibrido2011cache.xml'.split(','),
+
+      /**
+       * WMC predefined names
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'predefinedNames': 'callejerocacheado,callejero,ortofoto,idea,ortofoto09,callejero2011cache,ortofoto2011cache,hibrido2011cache'.split(','),
+
+      /**
+       * WMC context names
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'names': 'mapa callejero cache,mapa del callejero,mapa ortofoto,mapa idea,mapa ortofoto09,Callejero,Ortofoto,Híbrido'.split(',')
+   });
+
+   /**
+    * Default projection
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('DEFAULT_PROJ', 'EPSG:23030*m');
+
+   /**
+    * Predefined WMC files. It is composed of URL,
+    * predefined name and context name.
+    * @type {object}
+    * @public
+    * @api stable
+    */
+   M.config('geoprint', {
+      /**
+       * Printer service URL
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'URL': 'http://geoprint-sigc.juntadeandalucia.es/geoprint/pdf',
+
+      /**
+       * WMC predefined names
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'DPI': 150,
+
+      /**
+       * WMC context names
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'FORMAT': 'png',
+
+      /**
+       * WMC context names
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'TEMPLATE': 'A4 horizontal (Leyenda en una hoja)',
+
+      /**
+       * WMC context names
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'FORCE_SCALE': false
+   });
+
+   /**
+    * Predefined WMC files. It is composed of URL,
+    * predefined name and context name.
+    * @type {object}
+    * @public
+    * @api stable
+    */
+   M.config('panels', {
+      /**
+       * TODO
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'TOOLS': 'measurebar,getfeatureinfo'.split(','),
+
+      /**
+       * TODO
+       * @const
+       * @type {Array<string>}
+       * @public
+       * @api stable
+       */
+      'EDITION': 'drawfeature,modifyfeature,deletefeature,editattribute,savefeature,clearfeature'.split(',')
+   });
+   
+   /**
+    * Searchstreet service URL
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('SEARCHSTREET_URL', 'http://ws079.juntadeandalucia.es/EXT_PUB_CallejeroREST/buscarCallejero');
+   
+   /**
+    * Autocomplete municipality service URL
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('SEARCHSTREET_URLCODINEAUTOCOMPLETE', 'http://ws079.juntadeandalucia.es/EXT_PUB_CallejeroREST/autocompletarDireccionMunicipio');
+   
+   /**
+    * service URL check code INE
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('SEARCHSTREET_URLCOMPROBARINE', 'http://ws079.juntadeandalucia.es/EXT_PUB_CallejeroREST/comprobarCodIne');
+   
+   /**
+    * Autocomplete service URL
+    * @const
+    * @type {string}
+    * @public
+    * @api stable
+    */
+   M.config('AUTOCOMPLETADOR_URL', 'http://ws079.juntadeandalucia.es/EXT_PUB_CallejeroREST/autocompletarDireccion');
+})(window.M);
